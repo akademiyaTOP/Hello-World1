b0f8fa13eac99261ccb9af537600222e3370a2f2
akademiyaTOP <dk26.01.1999@yandex.ru>
2023-10-23 07:35:52 +0300

    Восстановил текст вывода

--- Hello World1/Hello World1.cpp
+++ Hello World1/Hello World1.cpp
@@ -6,7 +6,7 @@ int main()
     std::cout << "Pick number: ";
     int a;
     std::cin >> a;
-    std::cout << "Hallo World!\n";
+    std::cout << "Hello World!\n";
     std::cout << std::endl;
 
     foo(a);
